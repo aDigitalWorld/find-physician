4de62da6ebe901c5a3f5cd68c915bf69ed8bf0f2
Shawn Crigger <ithippyshawn@gmail.com>
2020-10-23 15:08:13 -0400

    first commit of server api:

--- server/a.php
+++
@@ -1,7 +0,0 @@
-<?php
-//var_dump($argv);
-
-if (isset($argv[1])) {
-  echo md5($argv[1]);
-}
-
--- server/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$pattern = '^(http|https)://^';
-$url = 'ftp://www.test.com';
-var_dump(preg_match($pattern,$url));
